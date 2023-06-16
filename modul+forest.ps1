070757d167906869e374c5b2de1d994ccf46800e
Simon O <98746432+SimonO2001@users.noreply.github.com>
2023-06-16 04:06:46 -0700

    Create modul+forest.ps1

---
+++ modul+forest.ps1
@@ -0,0 +1,16 @@
+# Importer modul
+Import-Module ADDSDeployment
+
+# Installere forest
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "kursus04.com" `
+-DomainNetbiosName "KURSUS23" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
