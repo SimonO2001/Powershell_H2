840a468c9a4a2046a0a3a6a479f4af535941d210
Simon O <98746432+SimonO2001@users.noreply.github.com>
2023-06-19 00:58:56 -0700

    Create UserAuto.ps1

---
+++ UserAuto.ps1
@@ -0,0 +1,21 @@
+$parentOUPath = Get-Content -Path Users.txt -TotalCount 1
+
+
+$users = Import-Csv -Path Users.txt -Delimiter "," -Header "Firstname", "Lastname", "Username", "Password", "DomainMail", "OUName" | Select-Object -Skip 1
+
+
+foreach ($user in $users) {
+    $firstname = $user.Firstname
+    $lastname = $user.Lastname
+    $username = $user.Username
+    $password = ConvertTo-SecureString $user.Password -AsPlainText -Force
+    $domainMail = $user.DomainMail
+    $ouName = $user.OUName
+    $ouPath = "OU=$ouName,$parentOUPath"
+
+
+    New-ADUser -GivenName $firstname -Surname $lastname -SamAccountName $username -AccountPassword $password -UserPrincipalName $username$domainMail -name "$firstname $lastname" -Enabled $true -PasswordNeverExpires $true -Path $ouPath
+
+
+
+}
